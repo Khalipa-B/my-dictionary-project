2990af8596e7dc1ea4bfb8a55d6d3e2ef9aa2715
Khalipa-B <khalipa.baba@gmail.com>
2025-05-17 19:56:35 +0200

    updated css

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,19 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div className="definition">{definition.definition}</div>
+      <section>
+        <h3>{props.meaning.partOfSpeech}</h3>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div key={index}>
+              <div className="definition">{definition.definition}</div>
 
-            <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} />
-          </div>
-        );
-      })}
+              <div className="example">{definition.example}</div>
+              <Synonyms synonyms={definition.synonyms} />
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
